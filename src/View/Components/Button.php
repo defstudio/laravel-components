e2ebc275b27eab97ec1045eccc3a1b9731b143e4
Fabio Ivona <fabio.ivona@defstudio.it>
2020-11-10 10:25:22 +0100

    fix

--- src/View/Components/Button.php
+++ src/View/Components/Button.php
@@ -22,6 +22,11 @@ class Button extends Component
     public string $method;
     public string $confirm;
     public string $icon;
+    public string $get;
+    public string $post;
+    public string $put;
+    public string $patch;
+    public string $delete;
 
     public function __construct(
         string $color = 'primary',
@@ -35,33 +40,38 @@ class Button extends Component
         string $delete = ''
     )
     {
-        $this->compute_href($get, $post, $put, $patch, $delete);
-
         $this->color = $color;
         $this->type = $type;
         $this->confirm = $confirm;
 
         $this->icon = $icon;
 
+        $this->get = $get;
+        $this->post = $post;
+        $this->put = $put;
+        $this->patch = $patch;
+        $this->delete = $delete;
+
+        $this->compute_href();
     }
 
-    private function compute_href(string $get, string $post, string $put, string $patch, string $delete): void
+    private function compute_href(): void
     {
 
-        if ($get) {
-            $this->href = $get;
+        if ($this->get) {
+            $this->href = $this->get;
             $this->method = self::METHOD_GET;
-        } else if ($post) {
-            $this->href = $post;
+        } else if ($this->post) {
+            $this->href = $this->post;
             $this->method = self::METHOD_POST;
-        } else if ($put) {
-            $this->href = $put;
+        } else if ($this->put) {
+            $this->href = $this->put;
             $this->method = self::METHOD_PUT;
-        } else if ($patch) {
-            $this->href = $patch;
+        } else if ($this->patch) {
+            $this->href = $this->patch;
             $this->method = self::METHOD_PATCH;
-        } else if ($delete) {
-            $this->href = $delete;
+        } else if ($this->delete) {
+            $this->href = $this->delete;
             $this->method = self::METHOD_DELETE;
         } else {
             $this->href = '';
