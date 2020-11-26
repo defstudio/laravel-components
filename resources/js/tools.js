5859d0cc820e52bec5337d5859b3e4c309934908
Fabio Ivona <fabio.ivona@defstudio.it>
2020-11-26 14:07:02 +0100

    add shuffle js tool
    Signed-off-by: Fabio Ivona <fabio.ivona@defstudio.it>

--- resources/js/tools.js
+++ resources/js/tools.js
@@ -152,6 +152,25 @@ deftools.spinner = {
 //<editor-fold desc="Numbers">
 deftools.numbers = {
     random: (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+    shuffle: array => {
+        //Fisher-Yates shuffle (http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle)
+
+        var m = array.length, t, i;
+
+        // While there remain elements to shuffle…
+        while (m) {
+
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+
+        return array;
+    }
 }
 //</editor-fold>
 
