fb2108042128b4f98f1bdd036db5d0d410120cd1
Fabio Ivona <fabio.ivona@defstudio.it>
2020-12-07 18:57:15 +0100

    fix notification
    Signed-off-by: Fabio Ivona <fabio.ivona@defstudio.it>

--- resources/js/components/notifications/Notification.vue
+++ resources/js/components/notifications/Notification.vue
@@ -3,8 +3,19 @@
          class="notification-item d-flex">
 
         <div class="item-content flex-grow-1">
-            <h4 class="item-title" v-text="notification.data.title"/>
-            <p class="item-info" v-text="notification.data.message"/>
+            <h4 class="item-title">{{ notification.data.title }}</h4>
+            <p class="item-info">{{ notification.data.message }}</p>
+
+            <small class="mt-4">
+                {{ notification.created_at | format_date }}
+            </small>
+
+            <div v-if="notification.data.actions" class="actions-container mt-2">
+                <a v-for="(url, label) in notification.data.actions"
+                   :href="url"
+                   class="btn btn-primary btn-sm"
+                >{{ label }}</a>
+            </div>
         </div>
 
         <div v-if="deletable" class="item-options d-flex flex-column">
--- resources/js/tools/vue/index.js
+++ resources/js/tools/vue/index.js
@@ -1,7 +1,15 @@
+import moment from "moment";
+
 window.Vue = require('vue');
 
 const files = require.context('../../components', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+Vue.filter('format_date', value => {
+    if (value) {
+        return moment(String(value)).format('MM/DD/YYYY HH:mm');
+    }
+})
 
 const app = new Vue({
     el: '#app',
