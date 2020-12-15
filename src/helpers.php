24c09d31f5856d99c5fb708de04f3766f620dc20
Fabio Ivona <fabio.ivona@defstudio.it>
2020-12-15 16:17:55 +0100

    add back() helper
    Signed-off-by: Fabio Ivona <fabio.ivona@defstudio.it>

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\RedirectResponse;
+
 if (!function_exists('str')) {
     /**
      * Get a new stringable object from the given string.
@@ -11,5 +13,24 @@ if (!function_exists('str')) {
     function str($string): \Illuminate\Support\Stringable
     {
         return \Illuminate\Support\Str::of($string);
+    }
+}
+
+if (!function_exists('back')) {
+    /**
+     * Create a new redirect response to the previous location.
+     *
+     * @param int   $status
+     * @param array $headers
+     * @param mixed $fallback
+     *
+     * @return RedirectResponse
+     */
+    function back($status = 302, $headers = [], $fallback = false)
+    {
+        if (request()->has('referer')) {
+            return app('redirect')->to(request()->referer, $status, $headers);
+        }
+        return app('redirect')->back($status, $headers, $fallback);
     }
 }
