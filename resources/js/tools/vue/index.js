9e865cc113f0c2ea3544283bad6c9df82eba9b64
Fabio Ivona <fabio.ivona@defstudio.it>
2021-01-13 16:09:01 +0100

    updated vue
    Signed-off-by: Fabio Ivona <fabio.ivona@defstudio.it>

--- resources/js/tools/vue/index.js
+++ resources/js/tools/vue/index.js
@@ -1,6 +1,11 @@
-import moment from "moment";
+/*
+ * Copyright (C) 2021. Def Studio
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Authors: Fabio Ivona <fabio.ivona@defstudio.it> & Daniele Romeo <danieleromeo@defstudio.it>
+ */
 
-window.Vue = require('vue');
+import moment from "moment";
+import Vue from "vue";
 
 const files = require.context('../../components', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
@@ -9,8 +14,4 @@ Vue.filter('format_date', value => {
     if (value) {
         return moment(String(value)).format('MM/DD/YYYY HH:mm');
     }
-})
-
-const app = new Vue({
-    el: '#app',
 })
