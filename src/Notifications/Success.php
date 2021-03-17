16848f4bb72c643374fb34304ddcd8b6f88fb8df
Fabio Ivona <fabio.ivona@defstudio.it>
2021-03-17 16:27:21 +0100

    adds notification static build method
    Signed-off-by: Fabio Ivona <fabio.ivona@defstudio.it>

--- src/Notifications/Success.php
+++ src/Notifications/Success.php
@@ -1,24 +1,35 @@
 <?php
-
+/*
+ * Copyright (C) 2021. Def Studio
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Authors: Fabio Ivona <fabio.ivona@defstudio.it> & Daniele Romeo <danieleromeo@defstudio.it>
+ */
 
 namespace DefStudio\Components\Notifications;
 
 
+use JetBrains\PhpStorm\Pure;
+
 class Success extends BaseNotification
 {
-
-
-    public function __construct(
+    #[Pure] public function __construct(
         string $title,
         string $message,
         iterable $actions = []
-    )
-    {
+    ) {
         return parent::__construct(
             $title,
             $message,
             'success',
             $actions,
         );
+    }
+
+    #[Pure] public static function build(
+        string $title,
+        string $message,
+        iterable $actions = []
+    ): static {
+        return new static($title, $message, $actions);
     }
 }
