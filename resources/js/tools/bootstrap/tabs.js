61cffe55251e4d7b0880adc0b0d1b265b74c620d
Fabio Ivona <fabio.ivona@defstudio.it>
2021-03-24 10:33:10 +0100

    typo
    Signed-off-by: Fabio Ivona <fabio.ivona@defstudio.it>

--- resources/js/tools/bootstrap/tabs.js
+++ resources/js/tools/bootstrap/tabs.js
@@ -1,9 +1,15 @@
+/*
+ * Copyright (C) 2021. Def Studio
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Authors: Fabio Ivona <fabio.ivona@defstudio.it> & Daniele Romeo <danieleromeo@defstudio.it>
+ */
+
 $(document).ready(function () {
-    let $tabs_to_be_remembered = $('.remeber-selected-tab');
+    let $tabs_to_be_remembered = $('.remember-selected-tab');
 
     $tabs_to_be_remembered.on('click', 'a[data-toggle="tab"]', function () {
         let $tab = $(this);
-        let $tabs = $tab.closest('.remeber-selected-tab');
+        let $tabs = $tab.closest('.remember-selected-tab');
 
         window.localStorage.setItem($tabs.attr('id') + "_active_tab", $tab.attr('href'));
     });
