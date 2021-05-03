dc862024f73bf1427e6ac1d4ebb702471a96c75d
Fabio Ivona <fabio.ivona@defstudio.it>
2021-05-03 12:02:50 +0200

    adds order-by-selection to <x-multiselect>
    Signed-off-by: Fabio Ivona <fabio.ivona@defstudio.it>

--- resources/js/tools/components/multiselect.js
+++ resources/js/tools/components/multiselect.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2021. Def Studio
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Authors: Fabio Ivona <fabio.ivona@defstudio.it> & Daniele Romeo <danieleromeo@defstudio.it>
+ */
+
 $(document).ready(function () {
     $('select[multiple]').selectpicker();
+
+    $(document).on('change', '.custom-select.order-by-selection', function () {
+        const $selector = $(this);
+        const selected = $selector.val();
+
+        if (selected.length === 1) {
+            const $selected_option = $selector.find(':selected');
+            $selected_option.remove();
+            $selector.prepend($selected_option);
+        }
+    })
 });
