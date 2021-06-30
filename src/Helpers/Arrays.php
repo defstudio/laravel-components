12cf4ad20bfa56e4c77e9117e324a040b6d21851
Fabio Ivona <fabio.ivona@defstudio.it>
2021-06-30 10:52:19 +0200

    adds Arr helper with stars in dot notation support

--- src/Helpers/Arrays.php
+++ src/Helpers/Arrays.php
@@ -9,8 +9,57 @@
 
 namespace DefStudio\Components\Helpers;
 
-class Arrays
+class Arrays extends \Illuminate\Support\Arr
 {
+    /**
+     * Overrides default forget method to add star support in dot notation:
+     *
+     * eg. Arr::forget('bar.*.foo);
+     *
+     * @param array $array
+     * @param array|string $keys
+     */
+    public static function forget(&$array, $keys)
+    {
+        $original = &$array;
+
+        $keys = (array)$keys;
+
+        if (count($keys) === 0) {
+            return;
+        }
+
+        foreach ($keys as $key) {
+            // if the exact key exists in the top-level, remove it
+            if (static::exists($array, $key)) {
+                unset($array[$key]);
+
+                continue;
+            }
+
+            $parts = explode('.', $key);
+
+            // clean up before each pass
+            $array = &$original;
+
+            while (count($parts) > 1) {
+                $part = array_shift($parts);
+
+                if (isset($array[$part]) && is_array($array[$part])) {
+                    $array = &$array[$part];
+                } elseif ($part === '*') {
+                    foreach ($array as &$arr) {
+                        static::forget($arr, implode('.', $parts));
+                    }
+                } else {
+                    continue 2;
+                }
+            }
+
+            unset($array[array_shift($parts)]);
+        }
+    }
+
     public static function are_equals($arr_1, $arr_2): bool
     {
         return $arr_1 == $arr_2;
