d40200e59e85e553313565695ac9cf90356e1586
Fabio Ivona <fabio.ivona@defstudio.it>
2021-09-24 15:00:29 +0200

    fix
    Signed-off-by: Fabio Ivona <fabio.ivona@defstudio.it>

--- src/View/Components/DateBox.php
+++ src/View/Components/DateBox.php
@@ -1,22 +1,23 @@
 <?php
+/*
+ * Copyright (C) 2021. Def Studio
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Authors: Fabio Ivona <fabio.ivona@defstudio.it> & Daniele Romeo <danieleromeo@defstudio.it>
+ */
 
 namespace DefStudio\Components\View\Components;
 
 use Carbon\Carbon;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 
 class DateBox extends Component
 {
     public function __construct(
         public Carbon $date
-    )
-    {
+    ) {
     }
 
-    public function render(): View|Factory|Htmlable|Closure|string|Application
+    public function render(): View
     {
         return view('def-components::date-box');
     }
