334687e3400b91c721c3932e1adb719d1ba41db6
Fabio Ivona <fabio.ivona@defstudio.it>
2021-11-04 12:27:40 +0100

    adds step in percent input

--- src/View/Components/Percent.php
+++ src/View/Components/Percent.php
@@ -7,23 +7,25 @@ namespace DefStudio\Components\View\Components;
 class Percent extends Input
 {
     public string $label;
-    public ?int $max;
-    public ?int $min;
+    public ?float $max;
+    public ?float $min;
     public $append = '%';
+    public ?int $step;
 
     public function __construct(
         string $name,
         string $label = '',
         string $id = '',
-        int $max = 100,
-        int $min = 0
-    )
-    {
+        float $max = 100,
+        float $min = 0,
+        int $step = 0
+    ) {
         $this->id = $id;
         $this->name = $name;
         $this->label = $label;
         $this->max = $max;
         $this->min = $min;
+        $this->step = $step;
     }
 
     /**
