7b238d4df5ae697093c02bda9549d6f1de42cfb1
mario <mario.gattolla@gmail.com>
2023-03-21 11:26:15 +0100

    fix popover

--- resources/js/tools/bootstrap/tooltips.js
+++ resources/js/tools/bootstrap/tooltips.js
@@ -1,19 +1,17 @@
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
     
-    if($('[data-toggle="popover"]').length > 0){
-        const $body = $('body')
-        $body.popover({
-            placement: 'bottom',
-            container: 'body',
-            html: true,
-            selector: '[data-toggle="popover"]',
-        });
-        
-        $body.click(function (e){
-            if (typeof $(e.target).data('original-title') == 'undefined' && $(e.target).parents('[data-toggle="popover"]').length === 0) {
-                $('[data-original-title]').popover('hide');
-            }
-        });
-    }
+    const $body = $('body')
+    $body.popover({
+        placement: 'bottom',
+        container: 'body',
+        html: true,
+        selector: '[data-toggle="popover"]',
+    });
+    
+    $body.click(function (e) {
+        if (typeof $(e.target).data('original-title') == 'undefined' && $(e.target).parents('[data-toggle="popover"]').length === 0) {
+            $('[data-original-title]').popover('hide');
+        }
+    });
 });
